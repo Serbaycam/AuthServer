abab0e0503e2566cd82adf69ff8f29f57eb79047
Serbay Çam <serbaycam@erateks.com>
2023-04-28 15:46:45 +0300

    Add project files.

---
+++ Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using AuthServer.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Data.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(150);
+            builder.Property(x => x.Price).IsRequired().HasColumnType("decimal(9,2)");
+        }
+    }
+}
